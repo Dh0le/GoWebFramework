39079a811c67a159a465fc2306e7752ad0a8ac95
Dh0le <i@zjiaqi.com>
2022-06-19 16:43:52 -0500

    Create Trie for web router

--- gee/router.go
+++ gee/router.go
@@ -1,30 +1,103 @@
 package gee
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
 	handlers map[string]HandlerFunc
+	roots map[string]*node
 }
 
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandlerFunc)}
+	return &router{
+		roots: make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
 }
 
+func parsePattern(pattern string)[]string{
+	// only one * is allow in this wildcard matching and parsing
+	vs := strings.Split(pattern,"/")
+	parts := make([]string,0)
+	for _,item := range vs{
+		if item != ""{
+			parts = append(parts, item)
+			if(item[0]=='*'){
+				break;
+			}
+		}
+	}
+	return parts
+}
+
+
+
 func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
-	log.Printf("Route %4s - %s", method, pattern)
+	//first we split pattern into parts
+	parts := parsePattern(pattern)
+	// get key for current handler function
 	key := method + "-" + pattern
+	// check if current method is supported
+	_,ok := r.roots[method]
+	if(!ok){
+		// add new method if current method is not supported
+		r.roots[method] = &node{}
+	}
+	// insert new routes for this method
+	r.roots[method].insert(pattern,parts,0)
+	// insert handler into hashtable
 	r.handlers[key] = handler
+}
+
+func (r *router)getRoute(method string, path string)(*node,map[string]string){
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root,ok := r.roots[method]
+
+	if(!ok){
+		// method not supported
+		return nil,nil;
+	}
+	n := root.search(searchParts,0)
+
+	if n != nil{
+		// we found a match path
+		// now we need to parse the parameter
+		parts := parsePattern(n.pattern)
+		for index, part := range parts{
+			if part[0] == ':'{
+				// found a paramter match
+				params[part[1:]] =  searchParts[index]
+
+			}else if part[0] == '*' && len(part) > 1{
+				// found a wild card matching
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n,params
+	}
+	return nil,nil
 }
 
 
 func(r *router)handle(c *Context){
-	key:= c.Method +"-"+c.Path
-	if handler,ok := r.handlers[key];ok{
-		handler(c);
-	}else{
-		c.String(http.StatusNotFound,"404 NOT FOUND:%s\n",c.Path)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
 	}
+	// key:= c.Method +"-"+c.Path
+	// if handler,ok := r.handlers[key];ok{
+	// 	handler(c);
+	// }else{
+	// 	c.String(http.StatusNotFound,"404 NOT FOUND:%s\n",c.Path)
+	// }
 }
+
+
---
+++ gee/trie.go
@@ -0,0 +1,83 @@
+package gee
+
+import "strings"
+
+
+type node struct{
+	// specific string
+	pattern string
+	// certain parts
+	part string
+	children []*node
+	// wild card matching
+	isWild bool
+}
+
+// match single node for insertion
+func (n* node)matchChild(part string)*node{
+	for _,child := range n.children{
+		if child.part == part || child.isWild{
+			return child
+		}
+	}
+	return nil
+}
+
+// match mutiple node for search
+func (n *node)matchChildren(part string)[]*node{
+	nodes := make([]*node, 0)
+	for _,child := range n.children{
+		if(child.part == part || child.isWild){
+			nodes = append(nodes,child)
+		}
+	}
+	return nodes
+}
+
+// insert new route into trie tree
+func(n *node)insert(pattern string, parts []string,height int){
+	if len(parts) == height{
+		n.pattern = pattern
+		return 
+	}
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil{
+		child = &node{part:part,isWild: part[0]==':'||part[0]=='*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern,parts,height+1)
+
+}
+
+// search route in the trie tree
+func(n *node)search(parts[]string,height int)*node{
+	if len(parts) == height || strings.HasPrefix(n.part,"*"){
+		// check if we reach the last part or the last part is a wildcard
+		if(n.pattern == ""){
+			//match failed
+			return nil
+		}
+		return n
+	}
+	// get current part and match all routes for current part
+	part := parts[height]
+	children := n.matchChildren(part)
+	// for each part we recursively search for next part
+	for _,child:=range children{
+		result := child.search(parts,height+1)
+		if(result != nil){
+			// match found
+			return result
+		}
+
+	}
+	// match failed
+	return nil
+
+}
+
+
+
+
+
